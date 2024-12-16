57f8a47f47b7da4d25e19b9a483c651203263c6d
MinCaiLi <limincai@aliyun.com>
2024-12-16 15:50:39 +0800

    初始化代码沙箱

---
+++ src/main/java/com/mincai/ikuncode/codesandbox/Application.java
@@ -0,0 +1,16 @@
+package com.mincai.ikuncode.codesandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author limincai
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/mincai/ikuncode/codesandbox/IkuncodeCodesandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mincai.ikuncode.codesandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IkuncodeCodesandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
